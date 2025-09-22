27f278aab18fcfe1ba171912573fd2a0f95f0b34
krllkrnv <krllkrnv@yandex.ru>
2025-09-22 17:06:13 +0300

    feat(AppDrag, AppDrop, constants, AppIcon): реализация draggable обертки и зоны сброса, создан компонент AppIcon, добавлены константы в constants для drag

--- demo/src/common/constants.js
+++ demo/src/common/constants.js
@@ -30,4 +30,8 @@ export const STATUSES = [
   export const DAY_IN_MILLISEC = DAY_IN_SEC * SEC;
   
   export const TAG_SEPARATOR = '#';
+
+  export const MOVE = 'move';
+  export const DATA_TRANSFER_PAYLOAD = 'payload';
+
   
