2f1d340f214828a69d664a9c091f70268dddccf6
krllkrnv <krllkrnv@yandex.ru>
2025-09-22 17:51:50 +0300

    feat(helpers, CounterButton, CounterValue, DemoCounter, DeskColumn, TaskCard, TaskCArdTags, HomeView: добавление компонентов для счетчика и задач, реализация логики управления колонками и задачами

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,51 @@
+<template>
+  <demo-container>
+    <div class="counter">
+      <counter-button @action="decrement">-</counter-button>
+
+      <counter-value :value="count" />
+
+      <counter-button @action="increment">+</counter-button>
+    </div>
+  </demo-container>
+</template>
+
+<script setup>
+import CounterValue from "./CounterValue.vue";
+import CounterButton from "./CounterButton.vue";
+import DemoContainer from "../components/DemoContainer.vue";
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const decrement = () => {
+  count.value = count.value - 1;
+}
+
+const increment = () => {
+  count.value = count.value + 1;
+}
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+
+  button {
+    margin: 0 10px;
+    font-size: 1.5rem;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    border: none;
+    background: #dd0000;
+    color: white;
+    cursor: pointer;
+
+    &:hover {
+      background: #ff0000;
+    }
+  }
+}
+</style>
