5c54b63cb9c01a8e2bf72a41944d17fab470dfa3
krllkrnv <krllkrnv@yandex.ru>
2025-10-05 19:38:51 +0300

    feat(App, routes): роутинг

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,16 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-import routes from '../routes'
+import routes from './routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("../components/DemoMenu.vue"),
-    },
-    ...routes,
-  ],
+  routes,
 });
 
 export default router;
