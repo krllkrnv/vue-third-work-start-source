8d1557b7eb274fcb948c7bf2a80296dfda97bc61
krllkrnv <krllkrnv@yandex.ru>
2025-10-25 23:45:13 +0300

    feat(stores): заполнение сторов

--- demo/src/stores/filters.js
+++ demo/src/stores/filters.js
@@ -6,6 +6,28 @@ export const useFiltersStore = defineStore('filters', {
 		users: [],
 		statuses: []
 	}),
-	getters: {},
-	actions: {},
+	getters: {
+		filters: state => {
+			const { search, users, statuses } = state
+			return {
+				search,
+				users,
+				statuses
+			}
+		}
+	},
+	actions: {
+		applyFilters ({ item, entity }) {
+			if (entity === 'search') {
+				this.search = item
+			} else {
+				const resultValues = [...this[entity]]
+				const itemIndex = resultValues.findIndex(el => el === item)
+				~itemIndex
+					? resultValues.splice(itemIndex, 1)
+					: resultValues.push(item)
+				this.$patch({ [entity]: resultValues })
+			}
+		}
+	},
 })
