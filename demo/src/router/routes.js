b5fb32e4332d7ce7e2079da7118403d305291dc0
krllkrnv <krllkrnv@yandex.ru>
2025-10-26 15:36:09 +0300

    feat(middlewares): добавление мидлвар

--- demo/src/router/routes.js
+++ demo/src/router/routes.js
@@ -1,3 +1,5 @@
+import { isAdmin } from '@/middlewares'
+
 export default [
   {
     path: "/login",
@@ -23,13 +25,19 @@ export default [
         path: "/tasks/create",
         name: "TaskCreate",
         component: () => import("../../../frontend/src/views/TaskCreate.vue"),
-        meta: { layout: "AppLayoutMain" },
+        meta: {
+          layout: "AppLayoutMain",
+          middlewares: [isAdmin],
+        },
       },
       {
         path: "/tasks/edit/:id",
         name: "TaskEdit",
         component: () => import("../../../frontend/src/views/TaskEdit.vue"),
-        meta: { layout: "AppLayoutMain" },
+        meta: {
+          layout: "AppLayoutMain",
+          middlewares: [isAdmin],
+        },
       },
     ],
   },
